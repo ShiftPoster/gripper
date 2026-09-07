46ee36277f795558e9550a13395a1f5b330a839a
Shift Poster <86fullers_secrets@icloud.com>
2026-09-07 01:47:55 -0400

    test: added data and more atlas tests

--- tests/test_atlas.py
+++ tests/test_atlas.py
@@ -1,14 +1,33 @@
 from pathlib import Path
 
-from gripper.atlas import CliAtlas
+import pytest
 
-SEARCH: Path = Path.cwd() / "tests" / "data" / "atlas_search.html"
-CD4: Path = Path.cwd() / "tests" / "data" / "atlas_cd4.html"
+from gripper.atlas import AtlasTable, CliAtlas, TableTitle
+
+SEARCH1: Path = Path.cwd() / "tests" / "data" / "atlas_search1.html"
+SEARCH2: Path = Path.cwd() / "tests" / "data" / "atlas_search2.html"
+CD4_1: Path = Path.cwd() / "tests" / "data" / "atlas_cd4_1.html"
+CD4_2: Path = Path.cwd() / "tests" / "data" / "atlas_cd4_2.html"
+SOLUTION_TABLE: AtlasTable = AtlasTable(
+    title='PROTEIN EXPRESSION AND LOCALIZATION',
+    rows={
+        'Tissue profilei ': 'Selective cytoplasmic expression in peripheral lymphocytes and subsets of cells in lymphoid tissues.',
+        'Tissue specificity (MS)': 'Tissue enhanced (Lymphoid tissue)',
+        'Cell type specificity (DVP)': 'Group enriched (B-cells, Macrophages, T-cells)',
+        'Subcellular locationi ': 'Localized to the Plasma membrane',
+        'Predicted locationi ': 'Membrane,  Intracellular (different isoforms)'
+    }
+)
 
 
-def test_reference():
-    refernce = CliAtlas.find_reference("CD4", SEARCH.read_text())
+@pytest.mark.parametrize("file", (SEARCH1, SEARCH2))
+def test_reference(file: Path):
+    refernce = CliAtlas.find_reference("CD4", file.read_text())
     assert "ENSG00000010610" == refernce
 
 
-def test_table(): ...
+@pytest.mark.parametrize("file", (CD4_1, CD4_2))
+def test_table(file: Path):
+    table = CliAtlas.get_table(file.read_text(), TableTitle.expression)
+    assert table
+    assert AtlasTable.parse(table) == SOLUTION_TABLE
