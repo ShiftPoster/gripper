31a43bc4184ccb0a005f28572895f19a95b22b13
Shift Poster <86fullers_secrets@icloud.com>
2026-09-23 21:53:48 -0400

    refactor: moved excel stuff to module

---
+++ src/gripper/excel.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+import pandas
+from loguru import logger
+from pydantic import BaseModel
+from rapidfuzz.process import extractOne
+
+
+@dataclass
+class FuzzMatch:
+    value: str
+    score: float
+    index: int
+
+    @classmethod
+    def extract(cls, query: str,  choices: list[str]):
+        return cls(*extractOne(query, choices))
+
+
+class ExcelSettings(BaseModel):
+    sheet: str | int = 0
+    column: str = "gene"
+    rows: int | None = 5
+
+    def read(self, file: Path) -> pandas.Series:
+        with pandas.ExcelFile(file) as frame:
+            logger.debug(f"{frame.sheet_names}")
+            if isinstance(self.sheet, int):
+                sheet = frame.sheet_names[self.sheet]
+            else:
+                strsheets = [_ for _ in frame.sheet_names if isinstance(_, str)]
+                sheet_match = FuzzMatch.extract(self.sheet, strsheets)
+                logger.debug(f"{sheet_match = }")
+                sheet = sheet_match.value
+
+            sheet = frame.parse(frame.sheet_names[0])
+            column_match = FuzzMatch.extract(self.column, sheet.columns.to_list())
+            logger.debug(f"{column_match = }")
+
+            if self.rows:
+                genes = sheet[column_match.value][:self.rows]
+            else:
+                genes = sheet[column_match.value]
+        return genes
+
+    def write(self, file: Path, data: dict[str, str]): ...
--- src/gripper/main.py
+++ src/gripper/main.py
@@ -1,18 +1,30 @@
+from pathlib import Path
+
+from pydantic import BaseModel, Field
 from pydantic_settings import CliApp, CliPositionalArg, CliSubCommand
 from rich import print
 
-from gripper.app import BaseApp
+from gripper.app import BaseApp, GripperSubcommand
 from gripper.atlas import CliAtlas
+from gripper.excel import ExcelSettings
+
+
+class CliPeek(BaseModel, GripperSubcommand):
+    excel: ExcelSettings = Field(default_factory=ExcelSettings)
+    file: CliPositionalArg[Path]
+
+    def main(self, *a, **k):
+        genes = self.excel.read(self.file)
+        print(genes)
 
 
 class GripperApp(BaseApp):
-    # file: CliPositionalArg[Path]
-    gene: CliPositionalArg[str]
     atlas: CliSubCommand[CliAtlas]
+    peek: CliSubCommand[CliPeek]
 
     def main(self):
-        atlas = self.get_subcommand(CliAtlas)
-        print(atlas.main(self.gene.upper()))
+        subcommand = self.get_subcommand()
+        subcommand.main()
 
 
 def main():
